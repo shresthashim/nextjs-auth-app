0c052c2248d03b1c30da5dbc6349583d60fdaa93
AsHim Shrestha <ashimsth89@gmail.com>
2024-02-04 12:43:08 +0545

    feat : login UI completed

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,11 +1,35 @@
 import React from 'react';
+import Link from "next/link";
 
 const LoginPage = () => {
     return (
-        <div>
-            
+        <div className="flex flex-col items-center justify-center min-h-screen py-2">
+
+
+            <label htmlFor="email">Email</label>
+            <input
+                className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
+                id="email"
+                type="text"
+
+                placeholder="Enter your email"
+            />
+            <label htmlFor="password">Password</label>
+            <input
+                className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
+                id="password"
+                type="password"
+
+                placeholder="Enter your password"
+            />
+            <button
+
+                className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600">Login
+                here
+            </button>
+            <Link href="/signup">Visit Signup page</Link>
         </div>
-    );
+    )
 };
 
 export default LoginPage;
