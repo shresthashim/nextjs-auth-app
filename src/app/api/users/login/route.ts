3af2e09647e6677605969ed0af46f6f7dc95b847
AsHim Shrestha <ashimsth89@gmail.com>
2024-02-07 21:32:59 +0545

    feat : create a jwt in login route for user cookie

--- src/app/api/users/login/route.ts
+++ src/app/api/users/login/route.ts
@@ -1,28 +1,42 @@
-import {connect} from "@/dbConfig/dbConfig";
+import { connect } from "@/dbConfig/dbConfig";
 import User from "@/models/userModel";
-import {NextRequest, NextResponse} from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import bcryptjs from "bcryptjs";
-
+import jwt from "jsonwebtoken";
 
 connect();
 
-export async  function POST(request: NextRequest) {
+export async function POST(request: NextRequest) {
     try {
         const reqBody = await request.json();
-        const {email, password} = reqBody;
+        const { email, password } = reqBody;
 
-        const user = await User.findOne({email});
-        if(!user) {
-            return NextResponse.json({error: "User doesn't exist!"}, {status: 400})
+        const user = await User.findOne({ email });
+        if (!user) {
+            return NextResponse.json({ error: "User doesn't exist!" }, { status: 400 });
         }
 
         const isMatch = await bcryptjs.compare(password, user.password);
-        if(!isMatch) {
-            return NextResponse.json({error: "Invalid credentials!"}, {status: 400})
+        if (!isMatch) {
+            return NextResponse.json({ error: "Invalid credentials!" }, { status: 400 });
         }
 
-    }
-    catch (error: any) {
-        return NextResponse.json({error: error.message}, {status: 500})
+        // Create token data
+        const tokenData = {
+            id: user._id,
+            email: user.email,
+            username: user.username,
+        };
+        const token = jwt.sign(tokenData, process.env.TOKEN_SECRET!, { expiresIn: "1d" });
+
+        // Set token as cookie in the response
+        const response = NextResponse.json({ message: "Login successful", success: true });
+        response.cookies.set('token', token, {
+            httpOnly: true,
+        });
+
+        return response;
+    } catch (error:any) {
+        return NextResponse.json({ error: error.message }, { status: 500 });
     }
 }
