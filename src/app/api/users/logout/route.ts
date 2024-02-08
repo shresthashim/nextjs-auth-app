733797c2b5fb23d68b770cae8c63c85bcc450e58
AsHim Shrestha <ashimsth89@gmail.com>
2024-02-08 08:36:00 +0545

    successful implementation of logout

--- src/app/api/users/logout/route.ts
+++ src/app/api/users/logout/route.ts
@@ -1,15 +1,20 @@
-import {NextResponse} from "next/server";
+import { NextResponse } from "next/server";
 
-export default function GET() {
 
+export async function GET() {
     try {
-
-        const response = NextResponse.json({message: "User logged out successfully"}, {status: 200});
-        response.cookies.set("token", "", {httpOnly: true})
+        const response = NextResponse.json(
+            {
+                message: "Logout successful",
+                success: true,
+            }
+        )
+        response.cookies.set("token", "",
+            { httpOnly: true, expires: new Date(0)
+            });
         return response;
-
     } catch (error: any) {
-        return NextResponse.json({error: error.message}, {status: 500});
+        return NextResponse.json({ error: error.message }, { status: 500 });
     }
 
 }
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,13 +1,32 @@
+'use client';
 import React from 'react';
-
+import axios from "axios";
+import {toast} from "react-hot-toast";
+import {useRouter} from "next/navigation";
 const ProfilePage = () => {
-    return (<div className="flex justify-center items-center h-screen">
-            <div className="text-center">
-                <h1 className="text-3xl font-bold mb-4">Profile Page</h1>
-                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Logout</button>
-            </div>
-        </div>
-    );
-};
+    const router = useRouter();
 
+    const logout = async () => {
+        try {
+            await axios.get("/api/users/logout");
+            toast.success("Logged out successfully");
+            router.push("/login");
+
+        } catch (e: any) {
+            console.log(e);
+            toast.error(e.message);
+
+        }
+    };
+        return (<div className="flex justify-center items-center h-screen">
+                <div className="text-center">
+                    <h1 className="text-3xl font-bold mb-4">Profile Page</h1>
+                    <button onClick={logout}
+                            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Logout
+                    </button>
+                </div>
+            </div>
+        );
+
+}
 export default ProfilePage;
