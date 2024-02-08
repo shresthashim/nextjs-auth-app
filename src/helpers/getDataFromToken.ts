0b27e973a32a419efe73ebe6c87bcd662c22c3e7
AsHim Shrestha <ashimsth89@gmail.com>
2024-02-08 17:56:51 +0545

    get data from token

---
+++ src/helpers/getDataFromToken.ts
@@ -0,0 +1,14 @@
+import {NextRequest} from "next/server";
+import jwt from "jsonwebtoken";
+
+export const getDataFromToken = (req: NextRequest) => {
+
+    try {
+
+        const token = req.cookies.get('token')?.value || '';
+        const data: any = jwt.verify(token, process.env.JWT_SECRET!);
+        return data.id;
+    } catch (error: any) {
+        throw new Error(error.message);
+    }
+}
