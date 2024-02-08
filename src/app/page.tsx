c5e00ccfce13404c114e9e7876e6a192c3799382
AsHim Shrestha <ashimsth89@gmail.com>
2024-02-08 09:01:25 +0545

    successful implementation of middleware

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
-import Image from "next/image";
 
 export default function Home() {
   return (
-   <></>
+      <div className="flex justify-center items-center h-screen">
+        <div className="text-3xl font-bold">Home</div>
+      </div>
   );
 }
