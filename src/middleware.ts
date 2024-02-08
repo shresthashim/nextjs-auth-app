7b76788be3d646249d6fb3819e4b349b1bb33ea7
AsHim Shrestha <ashimsth89@gmail.com>
2024-02-08 08:48:29 +0545

    initialise : a middleware with matchers path

---
+++ src/middleware.ts
@@ -0,0 +1,12 @@
+import {NextRequest, NextResponse} from "next/server";
+
+export function middleware(req: NextRequest) {
+    return NextResponse.redirect(new URL('/', req.url));
+}
+
+export const config = {
+    matcher: [
+        '/', '/profile', '/login', '/signup'
+    ]
+
+}
