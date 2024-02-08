868fdccb3e569978d09e2dce1f19625ae0676c9f
AsHim Shrestha <ashimsth89@gmail.com>
2024-02-08 08:17:28 +0545

    feat : create a logout button in profile page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
 const ProfilePage = () => {
-    return (
-        <div className=''>
-            <h1>Profile Page</h1>
-
+    return (<div className="flex justify-center items-center h-screen">
+            <div className="text-center">
+                <h1 className="text-3xl font-bold mb-4">Profile Page</h1>
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Logout</button>
+            </div>
         </div>
     );
 };
