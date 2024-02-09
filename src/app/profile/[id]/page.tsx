ac559fca3be1a648b15264f661bf73e1f2d934b3
AsHim Shrestha <ashimsth89@gmail.com>
2024-02-09 07:34:52 +0545

    create a new helper for mail

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,10 +1,10 @@
 'use client';
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 import Link from "next/link";
 import axios from "axios";
 
-const UserProfile = ({ params }: any) => {
-    const [user, setUser] = useState({ username: "", email: "", isAdmin: false });
+const UserProfile = ({params}: any) => {
+    const [user, setUser] = useState({username: "", email: "", isAdmin: false});
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
