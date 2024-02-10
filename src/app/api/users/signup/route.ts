85edb70f8356a270e6184470da2c58083784e72d
AsHim Shrestha <ashimsth89@gmail.com>
2024-02-10 12:58:41 +0545

    send verification email in signup

--- src/app/api/users/signup/route.ts
+++ src/app/api/users/signup/route.ts
@@ -2,6 +2,7 @@ import {connect} from "@/dbConfig/dbConfig";
 import User from "@/models/userModel";
 import {NextRequest, NextResponse} from "next/server";
 import bcryptjs from "bcryptjs";
+import {sendMail} from "@/helpers/mailer";
 
 
 connect()
@@ -35,6 +36,8 @@ export async function POST(request: NextRequest) {
         console.log(savedUser);
 
 //send verification email
+        await sendMail({email, emailType: "VERIFY", userId: savedUser._id})
+
 
         return NextResponse.json({
             message: "User created successfully",
